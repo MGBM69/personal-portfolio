6b21574757b1e87d139da60f4a4191ca865d466d
Banuka Malshan <bmagamulla@gmail.com>
2026-01-23 16:20:12 +0530

    Update view of navbar

--- src/app/nav-bar/nav-bar.ts
+++ src/app/nav-bar/nav-bar.ts
@@ -1,11 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [],
+  imports: [MenubarModule],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.css',
 })
-export class NavBar {
-
+export class NavBar implements OnInit {
+  items: MenuItem[] | undefined;
+  ngOnInit(): void {
+    this.items = [
+      {
+        label: 'Home',
+        icon: 'pi pi-home',
+      },
+      {
+        label: 'Features',
+        icon: 'pi pi-star',
+      },
+      {
+        label: 'Projects',
+        icon: 'pi pi-search',
+        items: [
+          {
+            label: 'Components',
+            icon: 'pi pi-bolt',
+          },
+          {
+            label: 'Blocks',
+            icon: 'pi pi-server',
+          },
+          {
+            label: 'UI Kit',
+            icon: 'pi pi-pencil',
+          },
+          {
+            label: 'Templates',
+            icon: 'pi pi-palette',
+            items: [
+              {
+                label: 'Apollo',
+                icon: 'pi pi-palette',
+              },
+              {
+                label: 'Ultima',
+                icon: 'pi pi-palette',
+              },
+            ],
+          },
+        ],
+      },
+      {
+        label: 'Contact',
+        icon: 'pi pi-envelope',
+      },
+    ];
+  }
 }
