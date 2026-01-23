36d1d15db482cf62904adc3479e9751386cb14aa
Banuka Malshan <bmagamulla@gmail.com>
2026-01-23 15:33:37 +0530

    Create nav-bar component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
+import { NavBar } from './nav-bar/nav-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, NavBar],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
