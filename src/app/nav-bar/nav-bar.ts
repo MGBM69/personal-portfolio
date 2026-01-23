8485166911f6961c44d92af4eecec4c1d418c096
Banuka Malshan <bmagamulla@gmail.com>
2026-01-23 18:29:08 +0530

    Adding Routes to the respective components

--- src/app/nav-bar/nav-bar.ts
+++ src/app/nav-bar/nav-bar.ts
@@ -1,32 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { MenubarModule } from 'primeng/menubar';
-import { MenuItem } from 'primeng/api';
+import { MenuItem, PrimeIcons } from 'primeng/api';
+import { DrawerModule } from 'primeng/drawer';
+import { ButtonModule } from 'primeng/button';
+import { NgFor } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [MenubarModule],
+  imports: [MenubarModule, DrawerModule, ButtonModule, NgFor, RouterLink],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.css',
 })
 export class NavBar implements OnInit {
   items: MenuItem[] | undefined;
+  drawerVisible: boolean = false;
   ngOnInit(): void {
     this.items = [
       {
         label: 'Home',
         icon: 'pi pi-home',
+        routerLink: '/',
       },
       {
         label: 'Portfolio',
         icon: 'pi pi-star',
+        routerLink: '/portfolio',
       },
       {
         label: 'Resume',
         icon: 'pi pi-star',
+        routerLink: '/resume',
       },
       {
         label: 'Projects',
         icon: 'pi pi-search',
+        routerLink: '/projects',
         items: [
           {
             label: 'Components',
@@ -59,6 +68,7 @@ export class NavBar implements OnInit {
       {
         label: 'Contact',
         icon: 'pi pi-envelope',
+        routerLink: '/contact',
       },
     ];
   }
