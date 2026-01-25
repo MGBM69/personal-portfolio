863a5f4973f01eba4349855b4955219599a49b75
Banuka Malshan <bmagamulla@gmail.com>
2026-01-25 13:48:16 +0530

    Implement ThoughtService

--- frontend/src/app/Services/thought-service.ts
+++ frontend/src/app/Services/thought-service.ts
@@ -1,8 +1,22 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Thought } from '../model/thought';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ThoughtService {
-  
+  $apiUrl = `${environment.apiUrl}/thoughts`;
+  $http = inject(HttpClient);
+
+  //submit thought to backend
+  submitThought(thought: Thought): Observable<Thought> {
+    return this.$http.post<Thought>(this.$apiUrl, thought);
+  }
+  // return thoughts from backend
+  getThoughts(): Observable<Thought[]> {
+    return this.$http.get<Thought[]>(this.$apiUrl);
+  }
 }
