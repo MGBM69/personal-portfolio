7337e63b9b1f42545bda280cb6c4b5bda1fca4a6
Banuka Malshan <bmagamulla@gmail.com>
2026-02-04 11:45:36 +0530

    add Angular fire library to the project

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -10,6 +10,8 @@ import Aura from '@primeuix/themes/aura';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -23,5 +25,17 @@ export const appConfig: ApplicationConfig = {
         preset: Aura,
       },
     }),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'auth-portfolio-ecce5',
+        appId: '1:221154173251:web:55a84e437e70b03231cff6',
+        storageBucket: 'auth-portfolio-ecce5.firebasestorage.app',
+        apiKey: 'AIzaSyAO6V7mMuePyLj52GGLJVcssSfCBbtLAC0',
+        authDomain: 'auth-portfolio-ecce5.firebaseapp.com',
+        messagingSenderId: '221154173251',
+        measurementId: 'G-1HX36FZGCW',
+      }),
+    ),
+    provideAuth(() => getAuth()),
   ],
 };
