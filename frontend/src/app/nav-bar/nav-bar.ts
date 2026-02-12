0bff278e5cdf8ad98b79917c4990f29c9dc8225c
Banuka Malshan <bmagamulla@gmail.com>
2026-02-12 18:32:36 +0530

    remove project section from navbar

--- frontend/src/app/nav-bar/nav-bar.ts
+++ frontend/src/app/nav-bar/nav-bar.ts
@@ -33,39 +33,39 @@ export class NavBar implements OnInit {
         icon: 'pi pi-star',
         routerLink: '/resume',
       },
-      {
-        label: 'Projects',
-        icon: 'pi pi-search',
-        routerLink: '/projects',
-        items: [
-          {
-            label: 'Components',
-            icon: 'pi pi-bolt',
-          },
-          {
-            label: 'Blocks',
-            icon: 'pi pi-server',
-          },
-          {
-            label: 'UI Kit',
-            icon: 'pi pi-pencil',
-          },
-          {
-            label: 'Templates',
-            icon: 'pi pi-palette',
-            items: [
-              {
-                label: 'Apollo',
-                icon: 'pi pi-palette',
-              },
-              {
-                label: 'Ultima',
-                icon: 'pi pi-palette',
-              },
-            ],
-          },
-        ],
-      },
+      // {
+      //   label: 'Projects',
+      //   icon: 'pi pi-search',
+      //   routerLink: '/projects',
+      //   items: [
+      //     {
+      //       label: 'Components',
+      //       icon: 'pi pi-bolt',
+      //     },
+      //     {
+      //       label: 'Blocks',
+      //       icon: 'pi pi-server',
+      //     },
+      //     {
+      //       label: 'UI Kit',
+      //       icon: 'pi pi-pencil',
+      //     },
+      //     {
+      //       label: 'Templates',
+      //       icon: 'pi pi-palette',
+      //       items: [
+      //         {
+      //           label: 'Apollo',
+      //           icon: 'pi pi-palette',
+      //         },
+      //         {
+      //           label: 'Ultima',
+      //           icon: 'pi pi-palette',
+      //         },
+      //       ],
+      //     },
+      //   ],
+      // },
       {
         label: 'Contact',
         icon: 'pi pi-envelope',
