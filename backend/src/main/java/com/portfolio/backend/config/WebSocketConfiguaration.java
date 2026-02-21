528971edead9fd72c5a4ad5365e09dcf97454b54
Banuka Malshan <bmagamulla@gmail.com>
2026-02-21 13:44:19 +0530

    Implement WebSocketConfiguaration.java class with WebSocketMessageBrokerConfigurer interface and Overeide registerStompEndpoints() method and configureMessageBroker() method . 1. Angular can connecs through http://localhost:8080/socket .
    2. Angular can subscribe "/server2client/thoughts" endpoint to get thoughts real time

---
+++ backend/src/main/java/com/portfolio/backend/config/WebSocketConfiguaration.java
@@ -0,0 +1,17 @@
+package com.portfolio.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfiguaration implements WebSocketMessageBrokerConfigurer {
+	
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/socket").setAllowedOrigins("http://localhost:4200").withSockJS();
+	}
+
+}
