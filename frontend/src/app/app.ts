741cfeb7a1b0cc2b99840c0740e67c3338ab1c96
Banuka Malshan <bmagamulla@gmail.com>
2026-02-22 20:10:00 +0530

    change background and add footer to the project

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { NavBar } from './nav-bar/nav-bar';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Header, NavBar],
+  imports: [RouterOutlet, Header, NavBar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
