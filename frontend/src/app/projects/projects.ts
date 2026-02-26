f461149e6896104ab01218747057aedecf14910d
Banuka Malshan <bmagamulla@gmail.com>
2026-02-26 15:33:10 +0530

    import  wildx project component to the main project page.

--- frontend/src/app/projects/projects.ts
+++ frontend/src/app/projects/projects.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { PortfolioProject } from '../projects-list/portfolio-project/portfolio-project';
+import { Wildxf1Project } from '../projects-list/wildxf1-project/wildxf1-project';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [PortfolioProject],
+  imports: [PortfolioProject, Wildxf1Project],
   templateUrl: './projects.html',
   styleUrl: './projects.css',
 })
